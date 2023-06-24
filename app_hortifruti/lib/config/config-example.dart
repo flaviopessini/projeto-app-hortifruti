9053673e93a545ec982ccb496c2e4b867d024ae0
flaviopessini <flavio.p3.2012@gmail.com>
2023-06-24 17:13:37 -0300

    arquivo de config

--- app_hortifruti/lib/config/config-example.dart
+++ app_hortifruti/lib/config/config-example.dart
@@ -1,3 +1,8 @@
+///
+/// Arquivo destino a configurações.
+///
+/// Renomear o arquivo para 'config.dart'
+
 class Config {
   static const String port = '3333';
   static const String ipAddress = 'http://localhost:$port/';
